5f579d6db081bd722af4f39418a9b93095b13e2b
Fearocanity <91414643+fearocanity@users.noreply.github.com>
2022-12-24 19:34:13 +0900

    update to latest

--- frame_main.sh
+++ frame_main.sh
@@ -9,9 +9,9 @@ time_started="$(TZ='Asia/Tokyo' date)"
 
 # Main Loop
 for ((i=1;i<=fph;i++)); do
-    bash ./frameposter.sh "${1}" "${2}" || exit 1
+    bash ./frameposter.sh "${1}" "${2}" || bash img_process.sh "failed" "${time_started}" || exit 1
     sleep "$((mins * 60))"
 done
 
 time_ended="$(TZ='Asia/Tokyo' date)"
-convert -fill white -background "darkgreen" -gravity center -pointsize 72 -font "trebuc.ttf" label:"\ [√] Frame ${prev_frame}-$((prev_frame+fph-1)) was posted " -pointsize 25 label:"Time started: ${time_started}\nTime ended: ${time_ended}" -append -bordercolor "darkgreen" -border 30 status/status.jpg
+bash img_process.sh "success" "${prev_frame}" "$((prev_frame+fph-1))" "${time_started}" "${time_ended}"
