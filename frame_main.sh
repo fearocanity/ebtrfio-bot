aaadc79dc23aec3242d4f8e9ed056ea2eeb9c124
Fearocanity <91414643+fearocanity@users.noreply.github.com>
2023-03-28 20:25:26 +0800

    comp: use sed instead of printf

--- frame_main.sh
+++ frame_main.sh
@@ -18,7 +18,7 @@ lim_frame="$((prev_frame+fph-1))"
 [[ "${prev_frame}" -gt "${total_frame}" ]] && prev_frame="${total_frame}"
 
 time_ended="$(TZ='Asia/Tokyo' date)"
-ovr_all="$(LC_NUMERIC=en_US printf "%'.f\n" "$(<./counter_n.txt)")"
+ovr_all="$(sed -E ':L;s=\b([0-9]+)([0-9]{3})\b=\1,\2=g;t L' counter_n.txt)"
 abt_txt="$(printf '%s\n%s' "Chopped 3.5 FPS, Posting 15 Frames every 2 hours." "Total of \"${ovr_all}\" frame was successfully posted!!")"
 bash img_process.sh "success" "${prev_frame}" "${lim_frame}" "${time_started}" "${time_ended}"
 curl -sLk -X POST "https://graph.facebook.com/me/?access_token=${1}" --data-urlencode "about=${abt_txt}" -o /dev/null || true
