7c7565692815cf1dd89f4b3094bbb0e45483848c
Fearocanity (けろさん) <91414643+fearocanity@users.noreply.github.com>
2024-02-14 08:44:25 +0900

    S

--- frameposter.sh
+++ frameposter.sh
@@ -36,7 +36,7 @@ rc_location=./fb/tmprc.jpg
 : "${vid_totalfrm:=}"
 
 # Hardcoded Scrapings only Supported on ass subs by Erai Raws
-locationsub=(./fb/bocchiep12_en.ass ./fb/bocchiep12_jp.srt)
+locationsub=(./fb/bocchiep12_en.ass)
 
 # Temp Variables
 is_empty="1"
